43b542b37c4ac6b2386fba62065e1669ad5602bf
carolinesom <somcaroline@gmail.com>
2022-09-15 17:49:51 +0200

    login vue & routes

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,11 +1,22 @@
+import "bootstrap/dist/css/bootstrap.css"
+import "bootstrap/dist/js/bootstrap.js"
 import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createRouter, createWebHistory } from 'vue-router'
 
-import './assets/main.css'
+
+import App from './App.vue'
+import Login from './components/Login.vue' 
+
+const routes = [
+    { path: '/login', component: Login },
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+})
 
 const app = createApp(App)
 
 app.use(router)
-
 app.mount('#app')
